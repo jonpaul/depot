b16a397355c6c098bb393e19b2679f49a70fda78
Jon-Paul Lussier Jr <jonpaul.lussier@gmail.com>
2011-02-23 22:11:16 -0500

    Lil' design for the homepage; functional tests for index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
-  validates :title, :uniqueness => true
+  validates :title, :uniqueness => true, :length => { :minimum => 10 }
   validates :image_url, :format => {
     :with => %r{\.(gif|jpg|png|jpeg)$}i,
     :message => 'must be a URL for GIF, JPG, JPEG, or PNG image.'
   }
-  
 end
