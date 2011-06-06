11418de10faceaac535312c554fc72e3c22b4b31
Jon-Paul Lussier Jr <jonpaul.lussier@gmail.com>
2011-06-05 20:23:36 -0400

    Totally forgot where I am

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -46,9 +46,9 @@ class LineItemsController < ApplicationController
     
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart,
-                      :notice => 'Line item was successfully created.') }
+        format.html { redirect_to(store_url) }
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
+        format.js   { @current_item = @line_item }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @line_item.errors, :status => :unprocessable_entity }
